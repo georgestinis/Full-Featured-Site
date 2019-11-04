534c8fcbc6aad522d5b438a2623112ba74fbac57
george stinis <george.stinis@yahoo.gr>
2019-11-04 22:46:33 +0200

    Update files

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\stini\desktop\projects\full_featured_site\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
